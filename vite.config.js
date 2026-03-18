44be9961016f9497e8035874439dc0f53be40d54
Marlon <marlonsilveira.dev@gmail.com>
2026-03-17 22:39:43 -0300

    Ajuste no vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-
+  base: 'https://marlonsilveiradev.github.io/bgrepresentacoes-web/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
